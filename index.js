4c922b07a69b5a71bd67859c9f08ed83db5323cb
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-19 18:32:55 +0200

    Added API endpoints and respective documentation
    - Added endpoints for interacting with users and movies
    - Added the respective documentation for the endpoints

--- index.js
+++ index.js
@@ -1,62 +1,211 @@
 const express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	uuid = require('uuid'),
 	morgan = require('morgan');
 
-const app = express();
+app.use(bodyParser.json());
+app.use(morgan('common'));
 
-// define array of movie objects that later get handed over in JSON
-let topTenMovies = [
+// define arrays of user and movie objects that later get handed over in JSON
+let users = [
 	{
-		title: 'Batman Begins',
-		director: 'Christopher Nolan',
+		id: 1,
+		name: 'Kim',
+		favoriteMovies: [],
 	},
 	{
-		title: 'The Dark Knight',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'The Dark Knight Rises',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Interstellar',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Tenet',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Inception',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Dunkirk',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Memento',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'The Prestige',
-		director: 'Christopher Nolan',
-	},
-	{
-		title: 'Insomnia',
-		director: 'Christopher Nolan',
+		id: 2,
+		name: 'Joe',
+		favoriteMovies: 'The Fountain',
 	},
 ];
 
-app.use(morgan('common'));
-app.use(express.static('public'));
+let movies = [
+	{
+		Title: 'The Fountain',
+		Description: 'Description 1',
+		Genre: {
+			Name: 'Drama',
+			Description: 'Description Genre Drama',
+		},
+		Director: {
+			Name: 'Director 1',
+			Bio: 'Bio Director 1',
+			Birth: 1969,
+		},
+	},
+	{
+		Title: 'Title 2',
+		Description: 'Description 2',
+		Genre: {
+			Name: 'Action',
+			Description: 'Description Genre Action',
+		},
+		Director: {
+			Name: 'Director 2',
+			Bio: 'Bio Director 2',
+			Birth: 1972,
+		},
+	},
+	{
+		Title: 'Title 3',
+		Description: 'Description 3',
+		Genre: {
+			Name: 'Drama',
+			Description: 'Description Genre Drama',
+		},
+		Director: {
+			Name: 'Director 3',
+			Bio: 'Bio Director 3',
+			Birth: 1980,
+		},
+	},
+	{
+		Title: 'Title 4',
+		Description: 'Description 4',
+		Genre: {
+			Name: 'Drama',
+			Description: 'Description Genre Drama',
+		},
+		Director: {
+			Name: 'Director 4',
+			Bio: 'Bio Director 4',
+			Birth: 1981,
+		},
+	},
+	{
+		Title: 'Title 5',
+		Description: 'Description 5',
+		Genre: {
+			Name: 'Drama',
+			Description: 'Description Genre Drama',
+		},
+		Director: {
+			Name: 'Director 5',
+			Bio: 'Bio Director 5',
+			Birth: 1982,
+		},
+	},
+];
 
-// GET requests
-app.get('/', (req, res) => {
-	res.send('Welcome to my Movie API!');
+// CREATE new user
+app.post('/users', (req, res) => {
+	const newUser = req.body;
+
+	if (newUser.name) {
+		newUser.id = uuid.v4();
+		users.push(newUser);
+		res.status(201).json(newUser);
+	} else {
+		res.status(400).send('New user requires name');
+	}
 });
 
+// UPDATE user
+app.put('/users/:id', (req, res) => {
+	const { id } = req.params;
+	const updatedUser = req.body;
+
+	let user = users.find((user) => user.id == id);
+
+	if (user) {
+		user.name = updatedUser.name;
+		res.status(200).json(user);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+// CREATE favorite movie
+app.post('/users/:id/:movieTitle', (req, res) => {
+	const { id, movieTitle } = req.params;
+
+	let user = users.find((user) => user.id == id);
+
+	if (user) {
+		user.favoriteMovies.push(movieTitle);
+		res.status(200).send(
+			`${movieTitle} has been added to user ${id}'s array`
+		);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+// DELETE favorite movie
+app.delete('/users/:id/:movieTitle', (req, res) => {
+	const { id, movieTitle } = req.params;
+
+	let user = users.find((user) => user.id == id);
+
+	if (user) {
+		user.favoriteMovies = user.favoriteMovies.filter(
+			(title) => title !== movieTitle
+		);
+		res.status(200).send(
+			`${movieTitle} has been removed from user ${id}'s array`
+		);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+// DELETE user
+app.delete('/users/:id', (req, res) => {
+	const { id } = req.params;
+
+	let user = users.find((user) => user.id == id);
+
+	if (user) {
+		users = users.filter((user) => user.id != id);
+		res.status(200).send(`User ${id} has been removed from the database`);
+	} else {
+		res.status(400).send('User not found');
+	}
+});
+
+// READ all movies
 app.get('/movies', (req, res) => {
-	res.json(topTenMovies);
+	res.status(200).json(movies);
+});
+
+// READ one movie by title
+app.get('/movies/:title', (req, res) => {
+	const { title } = req.params;
+	const movie = movies.find((movie) => movie.Title === title);
+
+	if (movie) {
+		res.status(200).json(movie);
+	} else {
+		res.status(404).send('Movie not in database');
+	}
+});
+
+// READ genre by name
+app.get('/movies/genre/:genreName', (req, res) => {
+	const { genreName } = req.params;
+	const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+
+	if (genre) {
+		res.status(200).json(genre);
+	} else {
+		res.status(404).send('Genre not in database');
+	}
+});
+
+// READ director by name
+app.get('/movies/directors/:directorName', (req, res) => {
+	const { directorName } = req.params;
+	const director = movies.find(
+		(movie) => movie.Director.Name === directorName
+	).Director;
+
+	if (director) {
+		res.status(200).json(director);
+	} else {
+		res.status(404).send('Director not in database');
+	}
 });
 
 // error handler
