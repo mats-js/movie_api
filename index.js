bacfbb4cdd172fb318d9f260105f4d50bcd9cb61
mats-js <100519629+mats-js@users.noreply.github.com>
2022-09-30 08:44:14 +0200

    Updated CORS to allow all domains

--- index.js
+++ index.js
@@ -14,24 +14,9 @@ const Movies = Models.Movie,
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
-// Import and use CORS
+// Import and use CORS but allow all domains to make requests
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (!origin) return callback(null, true);
-			if (allowedOrigins.indexOf(origin) === -1) {
-				// If a specific origin isn’t found on the list of allowed origins
-				let message =
-					`The CORS policy for this application doesn’t allow access from origin ` +
-					origin;
-				return callback(new Error(message), false);
-			}
-			return callback(null, true);
-		},
-	})
-);
+app.use(cors());
 
 // Import and use Passport
 let auth = require('./auth.js')(app);
